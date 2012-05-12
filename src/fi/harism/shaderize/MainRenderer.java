eff8a5004098482e4dd6de005d26867fc9f2071c
harism <harism@gmail.com>
2012-05-12 17:59:51 +0300

    MainRenderer shaders moved into one.

--- src/fi/harism/shaderize/MainRenderer.java
+++ src/fi/harism/shaderize/MainRenderer.java
@@ -1,6 +1,5 @@
 package fi.harism.shaderize;
 
-import java.nio.ByteBuffer;
 import java.util.Vector;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -23,24 +22,15 @@ public class MainRenderer implements GLSurfaceView.Renderer {
 	private final Vector<Renderer> mFilters = new Vector<Renderer>();
 
 	private float mFrameRate;
-	private ByteBuffer mFullQuadVertices;
 	private final ObjCamera mObjCamera = new ObjCamera();
 
 	private final ObjScene mObjScene = new ObjScene();
 	private final boolean mShaderCompilerSupported[] = new boolean[1];
 	private final Shader mShaderCopy = new Shader();
 
-	private final Shader mShaderTransform = new Shader();
 	private long mTimeLastRender;
 	private long mTimeSwitchStart;
 	private int mWidth, mHeight;
-
-	public MainRenderer() {
-		// Create full scene quad buffer.
-		final byte FULL_QUAD_COORDS[] = { -1, 1, -1, -1, 1, 1, 1, -1 };
-		mFullQuadVertices = ByteBuffer.allocateDirect(4 * 2);
-		mFullQuadVertices.put(FULL_QUAD_COORDS).position(0);
-	}
 
 	public float getFramesPerSecond() {
 		return 1000f / mFrameRate;
@@ -105,14 +95,17 @@ public class MainRenderer implements GLSurfaceView.Renderer {
 			// Copy FBO to screen buffer.
 			GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
 			GLES20.glViewport(0, 0, mWidth, mHeight);
+
 			mShaderCopy.useProgram();
-			int aPosition = mShaderCopy.getHandle("aPosition");
-			GLES20.glVertexAttribPointer(aPosition, 2, GLES20.GL_BYTE, false,
-					0, mFullQuadVertices);
-			GLES20.glEnableVertexAttribArray(aPosition);
+
+			int uScale = mShaderCopy.getHandle("uScale");
+			int uAlpha = mShaderCopy.getHandle("uAlpha");
+			GLES20.glUniform1f(uScale, 1f);
+			GLES20.glUniform1f(uAlpha, 1f);
+
 			GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
 			GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mFboMain.getTexture(0));
-			GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+			Utils.renderQuad(mShaderCopy.getHandle("aPosition"));
 		}
 
 		if (mFilters.size() > 1 && mFilters.get(1).mInitilized) {
@@ -129,18 +122,16 @@ public class MainRenderer implements GLSurfaceView.Renderer {
 
 			GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
 			GLES20.glViewport(0, 0, mWidth, mHeight);
-			mShaderTransform.useProgram();
-			int aPosition = mShaderTransform.getHandle("aPosition");
-			int uScale = mShaderTransform.getHandle("uScale");
-			int uAlpha = mShaderTransform.getHandle("uAlpha");
+			mShaderCopy.useProgram();
+
+			int uScale = mShaderCopy.getHandle("uScale");
+			int uAlpha = mShaderCopy.getHandle("uAlpha");
 			GLES20.glUniform1f(uScale, 1f + (1f - t) * 3f);
 			GLES20.glUniform1f(uAlpha, t);
-			GLES20.glVertexAttribPointer(aPosition, 2, GLES20.GL_BYTE, false,
-					0, mFullQuadVertices);
-			GLES20.glEnableVertexAttribArray(aPosition);
+
 			GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
 			GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mFboMain.getTexture(0));
-			GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+			Utils.renderQuad(mShaderCopy.getHandle("aPosition"));
 		}
 
 	}
@@ -168,13 +159,10 @@ public class MainRenderer implements GLSurfaceView.Renderer {
 
 			// Instantiate shaders.
 			String vertexSource, fragmentSource;
-			vertexSource = Utils.loadRawResource(mContext, R.raw.copy_vs);
-			fragmentSource = Utils.loadRawResource(mContext, R.raw.copy_fs);
-			mShaderCopy.setProgram(vertexSource, fragmentSource);
-			vertexSource = Utils.loadRawResource(mContext, R.raw.transform_vs);
+			vertexSource = Utils.loadRawResource(mContext, R.raw.main_copy_vs);
 			fragmentSource = Utils
-					.loadRawResource(mContext, R.raw.transform_fs);
-			mShaderTransform.setProgram(vertexSource, fragmentSource);
+					.loadRawResource(mContext, R.raw.main_copy_fs);
+			mShaderCopy.setProgram(vertexSource, fragmentSource);
 		} catch (Exception ex) {
 			showToast(ex.getMessage());
 		}
